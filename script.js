1764e0b84375c475e1e0c4fb20285a219ea6dc9d
Charles Emmanuel <emmaxcharles123@gmail.com>
2023-02-13 22:34:48 +0100

    Adding all other segment

--- script.js
+++
@@ -1,9 +0,0 @@
-// window.onresize = function() {
-//     if (window.innerWidth <= 768) {
-//       document.getElementsByTagName("link")[0].href = "mobile.css";
-//       document.getElementById("main").innerHTML = "Mobile HTML Content";
-//     } else {
-//       document.getElementsByTagName("link")[0].href = "desktop.css";
-//       document.getElementById("main").innerHTML = "Desktop HTML Content";
-//     }
-//   };
